69a3e81c5309b3bd6d92ce55cb48d6da658ee7af
Faizaan Sakib <ss16161@my.bristol.ac.uk>
2017-04-09 22:22:35 +0400

    Created simple Mr.X AI which moves by valuing each move with a score according to how may detectives there are capable of reaching the move’s destination within the next move. (Only checking for neighbouring nodes)

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MrX_AI.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MrX_AI.java
@@ -1,16 +1,24 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Set;
 import java.util.function.Consumer;
 
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.scotlandyard.ai.AIPool;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
+import uk.ac.bris.cs.scotlandyard.ai.Visualiser;
 import uk.ac.bris.cs.scotlandyard.model.Colour;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.model.Player;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
+
+
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 // Name the AI
 @ManagedAI("Mr X AI")
@@ -30,9 +38,57 @@ public class MrX_AI implements PlayerFactory {
 		@Override
 		public void makeMove(ScotlandYardView view, int location, Set<Move> moves, Consumer<Move> callback) {
 			// TODO do something interesting here; find the best move
+			System.out.println(moves);
 			// Picks a random move
-			callback.accept(new ArrayList<>(moves).get(random.nextInt(moves.size())));
+			ArrayList<Integer> scoreForMoves = new ArrayList<>();
 
+			// Add a score to an array for each move possible
+			for (Move move : moves)
+			{
+				if (move instanceof TicketMove)
+					scoreForMoves.add(score(view, ((TicketMove) move).destination()));
+				else if (move instanceof DoubleMove)
+					scoreForMoves.add(score(view, ((DoubleMove) move).finalDestination()));
+			}
+
+			// If no better scored moves are found, use least worst move
+			int bestScore = scoreForMoves.get(0);
+			for (int i = 1; i < scoreForMoves.size(); i++)
+			{
+				if (scoreForMoves.get(i) < bestScore)
+					bestScore = scoreForMoves.get(i);
+			}
+			for (int i = 0; i < scoreForMoves.size(); i++)
+			{
+				if (scoreForMoves.get(i) == bestScore)
+					callback.accept(new ArrayList<>(moves).get(i));
+			}
 		}
+
+		private int score(ScotlandYardView view, int location)
+		{
+			// Calculates a score according to where the detectives are
+			int score = 0;
+			int detectiveLocation;
+
+			// Get all nodes that are connected to MrX's current location
+			Collection<Edge<Integer, Transport>> accessibleNodes = view.getGraph().getEdgesFrom(view.getGraph().getNode(location));
+
+			// Cycles through each connected node to see if there is a detective there. If there is, add to the score
+			for (int i = 1; i < view.getPlayers().size(); i ++)
+			{
+				detectiveLocation = view.getPlayerLocation(view.getPlayers().get(i));
+				for (Edge <Integer, Transport> edge : accessibleNodes)
+				{
+					// Check if the connected node contains a detective
+					if (edge.destination().equals(view.getGraph().getNode(detectiveLocation)))
+					{
+						score++;
+					}
+				}
+			}
+			return score;
+		}
+
 	}
 }
